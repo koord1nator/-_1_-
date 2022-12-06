8f2da13aed6644b1b3c10f4e80e260551323fa01
koord1nator <119070101+koord1nator@users.noreply.github.com>
2022-12-06 19:48:08 +0300

    Add files via upload

---
+++ Задание2/Program.cs
@@ -0,0 +1,40 @@
+﻿    {
+ 
+        int a, b, c;
+ 
+        Console.WriteLine("Введите три числа ");
+ 
+        a = Convert.ToInt32(Console.ReadLine());
+ 
+        b = Convert.ToInt32(Console.ReadLine());
+         
+        c = Convert.ToInt32(Console.ReadLine());
+ 
+         if (a > b)     
+         if (a > c)
+
+        {
+ 
+            Console.WriteLine("{0} наибольшее число", a);
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", b);
+ 
+        } 
+
+         else
+ 
+        {
+ 
+            Console.WriteLine("{0} наибольшее число ", c);
+ 
+        }
+ 
+        Console.ReadLine();
+ 
+    }
